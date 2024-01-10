e7ef3f06bdaa395178e0afe2ebcbf7fd6e308e1f
global_gifts <fifa.1998.sava@mail.ru>
2024-01-10 12:59:37 +1000

    Этап 1

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const server = http.createServer((request, response) => {
     if ('hello' in queryObject) {
         if (!queryObject.hello) {               // Если параметр `hello` указан, но не передано `<name>`, то ответ **строка** "Enter a name", код ответа 400
             response.statusCode = 400;
+            response.setHeader('Content-Type', 'text/plain');
             response.end('Enter a name');
         } else {                                // Ответом на запрос `?hello=<name>` должна быть **строка** "Hello, <name>.", код ответа 200
             response.statusCode = 200;
@@ -19,17 +20,20 @@ const server = http.createServer((request, response) => {
         response.status = 200;
         response.statusMessage = 'OK';
         response.header = 'Content-Type: text/plain';
+        response.setHeader('Content-Type', 'text/plain');
         response.write('Hello, world!');
         response.end();
     } else if (request.url === '/?users') {     // Ответом на запрос `?users` должен быть **JSON** с содержимым файла `data/users.json`, код ответа 200
       response.status = 200;
       response.statusMessage = 'OK';
       response.header = 'Content-Type: application/json';
+      response.setHeader('Content-Type', 'application/json');
       response.write(getUsers());
       response.end();
       return;
     } else {
         response.statusCode = 500;
+        response.setHeader('Content-Type', 'text/plain');
         response.end();
     }
 });
@@ -37,9 +41,3 @@ const server = http.createServer((request, response) => {
 server.listen(3003, () => {
     console.log('Сервер запущен по адресу http://127.0.0.1:3003')
 })
-    // Написать обработчик запроса:
-    // + Ответом на запрос `?hello=<name>` должна быть **строка** "Hello, <name>.", код ответа 200
-    // + Если параметр `hello` указан, но не передано `<name>`, то ответ **строка** "Enter a name", код ответа 400
-    // + Ответом на запрос `?users` должен быть **JSON** с содержимым файла `data/users.json`, код ответа 200
-    // + Если никакие параметры не переданы, то ответ **строка** "Hello, World!", код ответа 200
-    // + Если переданы какие-либо другие параметры, то пустой ответ, код ответа 500
