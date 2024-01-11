939a7d84ec46333c292c275ec17f4f10d8276841
global_gifts <fifa.1998.sava@mail.ru>
2024-01-11 18:36:22 +1000

    Добила контроллеры books

--- src/controllers/books.js
+++ src/controllers/books.js
@@ -1,30 +1,58 @@
-const getAllUserBooks = require('../modules/books');
+const Book = require('../modules/books')
 
+// Получение всех книг
+const getBooks =  (request, response) => {
+    //get all books
+    Book.find({})
+        .then(book => {
+            response.status(200).send(book);
+        })
+        .catch(e => {
+            response.status(500).send(e.message);
+        });
+}
 
-const getBooks = (request, response) => {               // Получение всех читателей
-    response.status = 200;
-    response.statusMessage = 'OK';
-    response.header = 'Content-Type: application/json';
-    response.setHeader('Content-Type', 'application/json');
-    response.write(getAllUserBooks());
-    response.end();
-};
+// Получение книги по ID
+const getUserBook = (request, response) => {
+       const {book_id} = request.params;
+       return Book.findById(book_id).then(
+        (book) => {
+        if (!book) response.status(404).send("book not found")
+        else response.status(200).send(book)
+       }
 
-const getUserBook = (request, response) => {                // Получение читателя
+    ).catch(e =>  response.status(500).send(e.message))
+}
 
-};
+// Создание книги
+const createUserBook = (request, response) => {
+    return Book.create({...request.body}).then(
+        (book) => {response.status(201).send(book)}
+        
+    ).catch(e =>  response.status(500).send(e.message))
+}
 
-const createUserBook = (request, response) => {             // Создание нового читателя
+// Обновление данных книги по ID
+const updateUserBook = (request, response) => {
+    const {book_id} = request.params;
+    return Book.findByIdAndUpdate(book_id, {...request.body}).then(
+     (book) => {
+        if (!book) response.status(404).send("cannot update")
+        else response.status(200).send(book)
+       }
+ ).catch(e =>  response.status(500).send(e.message))
+}
 
-};
-
-const updateUserBook = (request, response) => {             // Обновление данных читателя
-
-};
-
-const deleteUserBook = (request, response) => {             // Удаление читателя
-
-};
+// Удаление книги по ID
+const deleteUserBook = (request, response) => {
+    const {book_id} = request.params;
+    return Book.findByIdAndDelete(book_id).then(
+     (book) => {
+        if (!book) response.status(404).send("cannot delete")
+        else response.status(200).send("sucess")
+       }
+ ).catch(e =>  response.status(500).send(e.message))
+}
 
 module.exports = {
     getBooks,
--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,33 +1,80 @@
 const User = require('../modules/users');
 
 // Главная
-const getMain = (req, res) => {
-    res.send('Hello, world!');
+const getMain = (request, response) => {
+    response.send('Hello, world!');
 };
- // Получение всех читателей
-const getUsers = (req, res) => {
-    res.send('Hello, users!');
-};
- // Получение читателеля по id
-const getUser = (req, res) => {
-    const { user_id } = req.params;
-    res.status(200);
-    res.send(`User ${ user_id }`);
-  };
-// Создание нового читателя
-const createUser = (req, res) => {            
-    return User.create({ ...req.body }).then(
-        (user) => { res.status(201).send(user) }
+
+// Получим всех читателей
+const getUsers = (request, response) => {
+    User.find({})
+      .then(user => {
+        response.status(200).send(user);
+      })
+      .catch(e => {
+        response.status(500).send(e.message);
+      });
+  }
+
+// Получим читателя по ID
+const getUser = (request, response) => {
+    const { user_id } = request.params;
+    User.findById(user_id).then(
+      (user) => {
+        if (!user) response.status(404).send("user not found")
+        else response.status(200).send(user)
+      }
     )
-};
+      .catch(e => {
+        response.status(500).send(e.message);
+      });
+  }
 
-const updateUser = (request, response) => {             // Обновление данных читателя
+// Создание нового читателя
+const createUser = (request, response) => {
+    const data = request.body;
+    User.create(data)
+      .then(
+        user => {
+          response.status(201).send(user);
+        }
+      )
+      .catch(e => {
+        response.status(500).send(e.message);
+      });
+  }
 
-};
-
-const deleteUser = (request, response) => {             // Удаление читателя
-
-};
+// Обновление данных читателя
+const updateUser = (request, response) => {
+    const { user_id } = request.params;
+    const data = request.body;
+    User.findByIdAndUpdate(user_id, data, { new: true, runValidators: true })
+      .then(
+        (user) => {
+          if (!user) response.status(404).send("cannot update")
+          else response.status(200).send(user)
+        }
+  
+      )
+      .catch(e => {
+        response.status(500).send(e.message);
+      });
+}
+  
+// Удаление читателя
+const deleteUser = (request, response) => {
+    const { user_id } = request.params;
+    User.findByIdAndDelete(user_id)
+      .then(
+        (user) => {
+          if (!user) response.status(404).send("cannot delete")
+          else response.status(200).send("sucess")
+        }
+      )
+      .catch(e => {
+        response.status(500).send(e.message);
+      });
+  }
 
 module.exports = {
     getMain,
